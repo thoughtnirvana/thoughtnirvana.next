d135399ccac10a458c3b9f720d5c4c93e0d84c40
Ujjawal <ujjawal@thoughtnirvana.com>
2016-03-03 22:52:52 +0530

    more style changes for ipad landscape and favicon

--- rb/js/tn.js
+++ rb/js/tn.js
@@ -8,10 +8,11 @@
       closeOnClick: true
     });
     $('.parallax').parallax();
-    setInterval(updateGradient,10);
+    setInterval(,10);
   }); // end of document ready
 
   var mn = $(".mainNav");
+  // min-height for header is 700
   $(window).scroll(function() {
     if($(this).scrollTop() > 700) {
       mn.addClass("mainNavScrolled");
@@ -115,25 +116,39 @@ navItems.click(function(e){
   e.preventDefault();
 });
 
-// Bind to scroll
 $(window).scroll(function(){
-   // Get container scroll position
    var fromTop = $(this).scrollTop()+mainNavHeight;
    
-   // Get id of current scroll item
    var cur = scrollItems.map(function(){
      if ($(this).offset().top < fromTop)
        return this;
    });
-   // Get the id of the current element
    cur = cur[cur.length-1];
    var id = cur && cur.length ? cur[0].id : "";
    
    if (lastId !== id) {
        lastId = id;
-       // Set/remove active class
        navItems
          .parent().removeClass("active")
          .end().filter("[href='#"+id+"']").parent().addClass("active");
    }                   
 });
+//***** Scrollspy feature end *****/
+
+//***** Animation *****/
+function animateIfVisible(el, sub_el) {
+  currentTop = $(window).scrollTop();
+  windowHeight = $(window).height();
+  currentBottom = currentTop + windowHeight;
+  elTop = $(el).offset().top;
+  elHeight = $(el).height();
+  elBottom = elTop + elHeight;
+
+  if(elTop >= currentTop && elBottom <= currentBottom) {
+    $(sub_el).addClass("fadeUp");
+  }
+}
+
+function addfadeInClass(el){
+  $(el).addClass("foo");
+}
