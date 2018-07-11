cc503d23d5ea0322735cb4f1878475190f09d411
Rahul Kumar <rahul@thoughtnirvana.com>
2018-07-11 13:52:23 +0530

    Refactored assets code

--- config/environment.rb
+++ config/environment.rb
@@ -1,16 +1,29 @@
 require 'ostruct'
+require 'sprockets'
+
 $LOAD_PATH.unshift Dir.pwd
 
 class Environment
   def self.config
     config = OpenStruct.new
+
     config.static_build_dir = '_build'
+
+
     config.assets = ['base.css'] +
       Dir['app/assets/images/**/*'].map{ |image_file| File.basename image_file }
     config.assets_sources = %w(app/assets/javascripts app/assets/stylesheets app/assets/images)
-    config.assets_output_dir = File.join(config.static_build_dir, 'assets')
+    config.assets_output_dir = 'assets'
+    config.assets_output_path = File.join(config.static_build_dir, 'assets')
     config.assets_manifest_name = 'manifest.json'
-    config.assets_manifest_path = File.join(config.assets_output_dir, config.assets_manifest_name)
+    config.assets_manifest_path = File.join(config.assets_output_path, config.assets_manifest_name)
+
+    config.sprockets_environment = Sprockets::Environment.new(Dir.pwd) do |env|
+      env.logger = Logger.new $stdout
+      env.logger.level = Logger::INFO
+    end
+    config.assets_sources.each { |source| config.sprockets_environment.append_path source }
+    config.assets_manifest = Sprockets::Manifest.new config.sprockets_environment.index, config.assets_manifest_path
 
     config
   end
--- lib/tasks/assets.rb
+++ lib/tasks/assets.rb
@@ -1,26 +1,16 @@
-require 'sprockets'
-
-environment = Sprockets::Environment.new(Dir.pwd) do |env|
-  env.logger = Logger.new $stdout
-  env.logger.level = Logger::INFO
-end
-Environment.config.assets_sources.each { |source| environment.append_path source }
-manifest = Sprockets::Manifest.new environment.index, Environment.config.assets_manifest_path
-
-
 namespace :assets do
   desc 'Compile assets'
   task :compile do
-    manifest.compile(*Environment.config.assets)
+    Environment.config.assets_manifest.compile(*Environment.config.assets)
   end
 
   desc 'Clean assets'
   task :clean do
-    manifest.clean
+    Environment.config.assets_manifest.clean
   end
 
   desc 'Remove all assets'
   task :clobber do
-    manifest.clobber
+    Environment.config.assets_manifest.clobber
   end
 end
