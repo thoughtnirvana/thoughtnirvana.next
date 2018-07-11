d21ebb414b59322f37687cb8ecb7f53630029aba
Rahul Kumar <rahul@thoughtnirvana.com>
2018-07-11 12:59:42 +0530

    Config for assets

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,15 @@ $LOAD_PATH.unshift Dir.pwd
 
 class Environment
   def self.config
-    config = {
-      static_build_dir: '_build'
-    }
-    OpenStruct.new config
+    config = OpenStruct.new
+    config.static_build_dir = '_build'
+    config.assets = ['base.css'] +
+      Dir['app/assets/images/**/*'].map{ |image_file| File.basename image_file }
+    config.assets_sources = %w(app/assets/javascripts app/assets/stylesheets app/assets/images)
+    config.assets_output_dir = File.join(config.static_build_dir, 'assets')
+    config.assets_manifest_name = 'manifest.json'
+    config.assets_manifest_path = File.join(config.assets_output_dir, config.assets_manifest_name)
+
+    config
   end
 end
